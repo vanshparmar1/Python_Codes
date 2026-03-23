cf573f8d5b25d4cc254f7212859e6176dd9493ef
vanshparmar1 <iamvanshparmar@gmail.com>
2026-03-23 20:08:10 +0530

    Add threading and multiprocessing examples for chai brewing; implement order taking and process management

---
+++ 12_threads_concurrency/01_threading.py
@@ -0,0 +1,24 @@
+import time
+import threading
+
+def take_order():
+    for i in range(1,4):
+        print(f"Taking order for #{i}")
+        time.sleep(2)
+
+def brew_chai():
+    for i in range(1,4):
+        print(f"brewing chai order for #{i}")
+        time.sleep(3)
+
+#create threads 
+order_thread = threading.Thread(target=take_order)
+brew_thread = threading.Thread(target=brew_chai)
+
+order_thread. start ()
+brew_thread.start()
+
+# wait for both to finish
+order_thread.join ()
+brew_thread.join()
+print(f"All orders taken and chai brewed")
